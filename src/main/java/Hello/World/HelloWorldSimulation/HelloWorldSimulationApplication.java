d1cb8b075fc077562d6d62908e4508b6dbbbd5e6
Prakhar Singh Rajput <prakharsinghrajput@amityonline.com>
2025-12-31 22:22:33 +0530

    initial commit by one and one only HELLOWORLDLOVER

---
+++ src/main/java/Hello/World/HelloWorldSimulation/HelloWorldSimulationApplication.java
@@ -0,0 +1,13 @@
+package Hello.World.HelloWorldSimulation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorldSimulationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWorldSimulationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Hello/World/HelloWorldSimulation/HelloWorldSimulationApplicationTests.java
@@ -0,0 +1,13 @@
+package Hello.World.HelloWorldSimulation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWorldSimulationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
